f491a79fdd378efe39607c774c0f45f24d210259
sejaflow <sejaflow2019@gmail.com>
2020-05-20 11:21:21 -0300

    criando a configuração para o static file

--- desafio/desafio/urls.py
+++ desafio/desafio/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+# heroku
+from django.conf import settings
+from django.conf.urls.static import static
+# Swagger
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -38,4 +42,4 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
