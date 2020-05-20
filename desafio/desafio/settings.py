c2d7bc0e9a3b2dcea4e1800afaf49e336601b5fb
sejaflow <sejaflow2019@gmail.com>
2020-05-20 11:44:27 -0300

    creating static folder in desain desafio

--- desafio/desafio/settings.py
+++ desafio/desafio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cl)bco*rr4lb$3h-ksk=a^pyvtc1l#&bkgnpek=c0l+uxrqub_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
